1871a49fbe99d2caff7dfc94c397062e87b9eaa7
Hannah Klein <hbklein@wisc.edu>
2024-11-19 15:38:25 -0600

    in class activity

--- app.js
+++ app.js
@@ -1,9 +1,100 @@
-fetch("https://jsonplaceholder.typicode.com/users")
+// task 1
+
+fetch("https://api.github.com/users/hbklein")
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json);
+    console.log(json.public_repos);
+    console.log(json.followers);
+  });
+
+fetch("https://api.github.com/users/hbklein/followers")
   .then((response) => response.json())
   .then((json) => {
     json.forEach((person) => {
-      console.log(person.id, person.name);
+      console.log(person.login);
     });
   });
 
-fetch("https://api.github.com/users/hbklein");
+// task 2
+// 1
+fetch("https://jsonplaceholder.typicode.com/comments")
+  .then((response) => response.json())
+  .then((json) => console.log(json.length));
+
+// 2
+fetch("https://jsonplaceholder.typicode.com/photos")
+  .then((response) => response.json())
+  .then((json) => console.log(json.length));
+
+//   3
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((json) => {
+    json.forEach((id) => {
+      console.log(id.name, id.email);
+    });
+  });
+
+//   4
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((json) => {
+    json.forEach((id) => {
+      console.log(id.name, id.address.city);
+    });
+  });
+
+//   5
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((response) => response.json())
+  .then((json) => {
+    let count = 0;
+    json.forEach((id) => {
+      if (id.completed == true) {
+        count += 1;
+      }
+    });
+    console.log(`${count} completed`);
+  });
+
+//   6
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((response) => response.json())
+  .then((json) => {
+    json.forEach((id) => {
+      if (id.completed == true) {
+        console.log(id.id, id.title);
+      }
+    });
+  });
+
+//   7
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((response) => response.json())
+  .then((json) => {
+    json.forEach((id) => {
+      if (id.completed == false) {
+        fetch(`https://jsonplaceholder.typicode.com/users/${id.userId}`)
+          .then((response) => response.json())
+          .then((json) => {
+            console.log(json.name);
+          });
+      }
+    });
+  });
+
+//   8
+fetch("https://jsonplaceholder.typicode.com/albums")
+  .then((response) => response.json())
+  .then((json) => {
+    json.forEach((id) => {
+      if (id.title == "quidem molestiae enim") {
+        fetch(`https://jsonplaceholder.typicode.com/users/${id.userId}`)
+          .then((response) => response.json())
+          .then((json) => {
+            console.log(json.name);
+          });
+      }
+    });
+  });
